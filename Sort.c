6b76922860dda6cffc5737f2d74b10386a1f02bf
Brendan ODowd <bodowd@worcester.edu>
2015-03-07 10:16:26 -0500

    Sorting algorithm added and printing correctly

--- Sort.c
+++ Sort.c
@@ -5,53 +5,36 @@
 int main() {
 
 	int num_of_rand; //the number of random numbers to be generated from user input
-	int* array_index; //pointer to index the above array
 	int a, b; //for loop indexes	
-	int* insert_sort();
+	int compare(); //the comparator function necessary for qsort()
 
 	printf("Please enter an integer: ");
 	scanf("%d",&num_of_rand);
 	srand(time(NULL)); //seed random number generator
 
 	int rand_array[num_of_rand]; //holds the randomly generated numbers
-	array_index = rand_array; //set the pointer to the initial value of the array
-
+	
 	for(a = 0; a < num_of_rand; a++) {
 		rand_array[a] = rand() % 100; //generate a random number between 0-99
-		printf("%d ",rand_array[a]); //output the numbers in the array
+		printf("%d ",rand_array[a]); //output the numbers in the array to console
 	}
 
 	printf("\n"); //console formatting 
 	
-	insert_sort(rand_array, num_of_rand);
+	qsort(rand_array, num_of_rand, sizeof(int), compare); //call the sorting function
 
 	for(b = 0; b < num_of_rand; b++) {
 		printf("%d ",rand_array[b]);
 	}	
 
 	printf("\n"); //console formatting
-	
+
 	return 0;
 
 }
 
-int* insert_sort(int* array, int size) {
-	
-	int i, j, x;
+int compare(const void *a, const void *b) {
 
-	for(i = 0; i > size; i++) {
-		
-		x = array[i];
-		j = i;
-		
-		while(j > 0 && array[j-1] > x) {
-			array[j] = array[j-1];
-			j--;
-		}
-		
-		array[j] = x;	
-	
-	}
+	return ( *(int*)a - *(int*)b );
 
-	return array;
 }
